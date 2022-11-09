f2e1c9f14ca3b06b9c6a90ed8a05c5a621b79486
goju2k <goju2k@naver.com>
2022-11-09 20:19:00 +0900

    saxx

--- messages/pull-request.js
+++ messages/pull-request.js
@@ -5,11 +5,20 @@ const prMessageCreator = async ()=>{
   //octokit
   const octo = github.getOctokit(process.env.GITHUB_TOKEN)
 
-  //pr info
-  const prInfo = (await octo.rest.pulls.get({
+  //pr params
+  const prParams = {
     owner:github.context.issue.owner,
     repo:github.context.issue.repo,
     pull_number:github.context.payload.pull_request.number
+  }
+
+  //pr info
+  const prInfo = (await octo.rest.pulls.get(prParams)).data
+
+  //pr commits
+  const prCommits = (await octo.rest.pulls.listCommits({
+    per_page:5,
+    ...prParams
   })).data
 
   //start
@@ -18,6 +27,39 @@ const prMessageCreator = async ()=>{
   //pr body content
   const content = prInfo.body
   content && content.length > 0 && (msg += "\n\n```"+content+"```")
+
+  //pr commit list
+  if(prCommits && prCommits.length > 0){
+
+    msg += '\n\n*Commit 목록 미리보기*'
+    for(let commit of prCommits){
+      msg += `\n- <${commit.commit.url}|${commit.commit.message}>`
+    }
+
+    if(prInfo.commits > prCommits.length){
+      msg += `\n 외 ${prInfo.commits - prCommits.length} 건`
+    }
+
+  }
+
+  //List pull requests files
+  const prFiles = (await octo.rest.pulls.listFiles({
+    per_page:5,
+    ...prParams
+  })).data
+
+  if(prFiles && prFiles.length > 0){
+
+    msg += '\n\n*변경된 파일 목록 미리보기*'
+    for(let file of prFiles){
+      msg += `\n- <${file.contents_url}|${file.filename}>`
+    }
+
+    if(prInfo.changed_files > prFiles.length){
+      msg += `\n 외 ${prInfo.changed_files - prFiles.length} 건`
+    }
+
+  }
 
   //pr link
   msg += `\n\n<${prInfo.html_url}>`
